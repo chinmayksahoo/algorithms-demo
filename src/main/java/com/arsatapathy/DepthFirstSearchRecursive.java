e7c4305f167fd22161b1a9f18bd5c220dd53ca0b
arsatapathy <ashish.satapathy2014@gmail.com>
2021-12-17 18:16:24 +0530

    Depth First Search Recursive

--- src/main/java/com/arsatapathy/DepthFirstSearchRecursive.java
+++ src/main/java/com/arsatapathy/DepthFirstSearchRecursive.java
@@ -1,12 +1,10 @@
 package com.arsatapathy;
 
 import java.util.Map;
-import java.util.Stack;
 
-public class DepthFirstSearch {
+public class DepthFirstSearchRecursive {
 
     public static void main(String[] args) {
-
         // Adjacency List
         final Map<Character, Character[]> graph = Map.of(
                 'a', new Character[]{'b', 'c'},
@@ -20,18 +18,10 @@ public class DepthFirstSearch {
         DFS(graph, 'a');
     }
 
-    public static void DFS(Map<Character, Character[]> graph, char start) {
-        Stack<Character> stack = new Stack<>();
+    public static void DFS(Map<Character, Character[]> graph, char source) {
+        System.out.print(source);
 
-        stack.push(start);
-
-        while (!stack.isEmpty()) {
-           char current = stack.pop();
-            System.out.print(current + " ");
-           for(char neighbour :graph.get(current)) {
-               stack.push(neighbour);
-           }
-        }
-
+        for(char neighbour :graph.get(source))
+            DFS(graph, neighbour);
     }
 }
