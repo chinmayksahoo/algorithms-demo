022f27b83f81ac88f8ad58164ca17b21031f0dc0
arsatapathy <ashish.satapathy2014@gmail.com>
2021-12-28 19:46:17 +0530

    Connected Compoments Count

--- src/main/java/com/arsatapathy/graph/ConnectedComponentsCount.java
+++ src/main/java/com/arsatapathy/graph/ConnectedComponentsCount.java
@@ -3,7 +3,6 @@ package com.arsatapathy.graph;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-import java.util.Stack;
 
 public class ConnectedComponentsCount {
 
@@ -15,27 +14,21 @@ public class ConnectedComponentsCount {
 
         for (Integer currentNode :graph.keySet()) {
             if (isConnected(graph, currentNode, visited))
-                count++;
+                count += 1;
         }
 
         return count;
     }
 
     public static boolean isConnected(Map<Integer, Integer[]> graph, Integer start, Set<Integer> visited) {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(start);
 
-        while(!stack.isEmpty()) {
-            Integer current = stack.pop();
+        if (visited.contains(start))
+            return false;
+        else
+            visited.add(start);
 
-            if (visited.contains(current))
-                return false;
-            else
-                visited.add(current);
-
-            for (Integer neighbour :graph.get(current))
-                stack.push(neighbour);
-        }
+        for (Integer neighbour :graph.get(start))
+            isConnected(graph, neighbour, visited);
 
         return true;
     }
--- src/test/java/com/arsatapathy/graph/ConnectedComponentsCountTest.java
+++ src/test/java/com/arsatapathy/graph/ConnectedComponentsCountTest.java
@@ -11,11 +11,13 @@ public class ConnectedComponentsCountTest {
     public void test_01() {
         // Adjacency List
         Map<Integer, Integer[]> graph = Map.of(
-                1, new Integer[]{2},
+                0, new Integer[]{8, 1, 5},
+                1, new Integer[]{0},
+                5, new Integer[]{0,8},
+                8, new Integer[]{0,5},
                 2, new Integer[]{3,4},
-                3, new Integer[]{},
-                4, new Integer[]{2},
-                5, new Integer[]{}
+                3, new Integer[]{2,4},
+                4, new Integer[]{3,2}
         );
 
         Assertions.assertEquals(2, ConnectedComponentsCount.count(graph));
