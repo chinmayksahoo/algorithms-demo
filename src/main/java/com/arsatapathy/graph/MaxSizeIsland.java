f06cd2737d5540c0e0ef55865ee2f11c4013c022
arsatapathy <ashish.satapathy2014@gmail.com>
2021-12-28 07:29:23 +0530

    Max Size Island

--- src/main/java/com/arsatapathy/graph/MaxSizeIsland.java
+++ src/main/java/com/arsatapathy/graph/MaxSizeIsland.java
@@ -4,11 +4,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class MaxSizeIsland {
-    public static void main(String[] args) {
-
-
-    }
-
 
     public static int maxIslandSize(char[][] grid) {
         int size = 0;
