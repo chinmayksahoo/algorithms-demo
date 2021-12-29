432c6b5ffe666e202f6f07348d0d2db200d28843
arsatapathy <ashish.satapathy2014@gmail.com>
2021-12-29 20:16:27 +0530

    Total Weight Problem

--- src/main/java/com/arsatapathy/tree/TotalWeight.java
+++ src/main/java/com/arsatapathy/tree/TotalWeight.java
@@ -3,14 +3,8 @@ package com.arsatapathy.tree;
 public class TotalWeight {
 
     public static int totalWeight(Node root) {
-        int sum = 0;
-
         if (root == null) return 0;
-
-        if (root.left != null)  sum += totalWeight(root.left);
-        if (root.right != null) sum += totalWeight(root.right);
-
-        return sum + root.getWeight();
+        else return root.getWeight() + totalWeight(root.left) + totalWeight(root.right);
     }
 
 }
