8614b3ab16a6deb3ac7034242375ab6ae22d4fc5
arsatapathy <ashish.satapathy2014@gmail.com>
2021-12-29 08:18:57 +0530

    Default Binary Tree Implementaion

--- src/test/java/com/arsatapathy/tree/DepthFirstTraversalRecursiveTest.java
+++ src/test/java/com/arsatapathy/tree/DepthFirstTraversalRecursiveTest.java
@@ -3,36 +3,12 @@ package com.arsatapathy.tree;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
-public class DepthFirstTraversalRecursiveTest {
+public class DepthFirstTraversalRecursiveTest extends BinaryTree {
 
     @Test
     public void test_01() {
-//        Binary Tree
-//             a
-//            / \
-//           b   c
-//          / \   \
-//         d   e   f
-
-        Node a = new Node('a');
-        Node b = new Node('b');
-        Node c = new Node('c');
-        Node d = new Node('d');
-        Node e = new Node('e');
-        Node f = new Node('f');
-
-        a.left = b;
-        a.right = c;
-
-        b.left = d;
-        b.right = e;
-
-        c.right = f;
-
         Assertions.assertEquals(List.of(a, b, d, e, c, f), DepthFirstTraversalRecursive.DFT(a));
     }
 
--- src/test/java/com/arsatapathy/tree/DepthFirstTraversalTest.java
+++ src/test/java/com/arsatapathy/tree/DepthFirstTraversalTest.java
@@ -5,32 +5,10 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-public class DepthFirstTraversalTest {
+public class DepthFirstTraversalTest extends BinaryTree {
 
     @Test
     public void test_01() {
-//        Binary Tree
-//             a
-//            / \
-//           b   c
-//          / \   \
-//         d   e   f
-
-        Node a = new Node('a');
-        Node b = new Node('b');
-        Node c = new Node('c');
-        Node d = new Node('d');
-        Node e = new Node('e');
-        Node f = new Node('f');
-
-        a.left = b;
-        a.right = c;
-
-        b.left = d;
-        b.right = e;
-
-        c.right = f;
-
         Assertions.assertEquals(List.of(a, b, d, e, c, f), DepthFirstTraversal.DFT(a));
     }
 
